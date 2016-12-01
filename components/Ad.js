f871284227aa9dfb2a63fc6a30e0a131ec38e62a
Ben McCormick <ben.mccormick@windsorcircle.com>
2016-12-01 11:09:35 -0500

    Add carbon ads

--- components/Ad.js
+++ components/Ad.js
@@ -1,51 +1,46 @@
 import React from 'react';
-import { Link } from 'react-router'
+import { Link, browserHistory } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
 import GoogleAd from 'react-google-ad'
+
 export class Ad extends React.Component {
 
-    // componentDidMount() {
-    // }
+    constructor(props) {
+        super(props);
+        this.state = {
+          url: null,
+        };
+    }
 
-    // render () {
-    //     return <div>
-    //         <div style= {{
-    //             maxHeight:'6.5rem',
-    //             display: 'flex',
-    //             justifyContent: 'flex-end',
-    //             background: '#F0F0F0',
-    //             padding: '0.2rem'
-    //         }} ref={el => this.ad = el} >
-    //         <div>
-    //         <a href="https://www.amazon.com/Effective-JavaScript-Specific-Software-Development/dp/0321812182/ref=as_li_ss_il?ie=UTF8&linkCode=li1&tag=benmccormicko-20&linkId=204d9fef609d10bc79cb424fc44fd1ab" target="_blank">
-    //         <img src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=0321812182&Format=_SL110_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=benmccormicko-20" />
-    //         <img src="https://ir-na.amazon-adsystem.com/e/ir?t=benmccormicko-20&l=li1&o=1&a=0321812182" width="1" height="1" alt="" style={{border:'none',  margin:'0px'}} />
-    //         </a>
-    //         </div>
-    //         <div style = {{
-    //             fontSize: '0.6em',
-    //             lineHeight: '1em',
-    //             maxWidth: '5rem',
-    //             padding: '0 0.5rem',
-    //             display: 'flex',
-    //             alignItems: 'center',
-    //             flexDirection: 'column',
-    //             justifyContent: 'space-between',
-    //             color: 'rgba(100, 100, 100, 0.701961)',
-    //         }}
-    //         >
-    //             <span>Please support this blog by shopping on Amazon</span>
-    //             <Link
-    //                 to={prefixLink('/amazonlinks')}
-    //             >
-    //              What is this?
-    //             </Link>
-    //         </div>
-    //         </div>
-    //     </div>
-    // }
+    insertScript() {
+        //build script
+        const script = document.createElement("script");
+        script.src = "//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=benmccormickorg";
+        script.type = "text/javascript";
+        script.id = "_carbonads_js";
+        script.async = true;
+        //remove anything in the container
+        while (this.container.hasChildNodes()) {
+            this.container.removeChild(this.container.lastChild);
+        }
+        //insert script
+        this.container.appendChild(script);
+    }
+
+    componentDidMount() {
+      browserHistory.listen( location =>  {
+        let url = location.pathname;
+        if (url !== this.state.url) {
+          this.insertScript();
+        }
+        this.setState({
+          url,
+        });
+      });
+    }
+
     render() {
-      return <GoogleAd client="ca-pub-3720609952480058" slot="8036002123" format="deprecated"/>
+      return <span ref={el => this.container = el}/>;
     }
 }
 
