5434b110bf95bcb7585ba508d0e371c459cf27dd
Ben McCormick <ben.mccormick@windsorcircle.com>
2017-06-03 17:45:03 -0400

    Cleanup feed code

--- utils/file_system.js
+++ utils/file_system.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 // From http://stackoverflow.com/questions/11293857/fastest-way-to-copy-file-in-node-js
-const copyFile = (source, target, cb) => {
+const copyFile = (sourcePath, targetPath, cb) => {
   let cbCalled = false;
 
   const done = (err) => {
@@ -10,16 +10,34 @@ const copyFile = (source, target, cb) => {
     }
   };
   //read stream
-  let rd = fs.createReadStream(source);
+  let rd = fs.createReadStream(__dirname + sourcePath);
   rd.on('error', err => done(err));
 
   //write stream
-  let wr = fs.createWriteStream(target);
+  let wr = fs.createWriteStream(__dirname + targetPath);
   wr.on('error', err => done(err));
   wr.on('close', ex => done());
   rd.pipe(wr);
 };
 
+const mkDir = path => {
+  try {
+    fs.mkdirSync(__dirname + path);
+  } catch (e) {
+        //this is probably fine, it may fail if the file already exists
+  }
+};
+
+const mkFile = (path, content) => {
+  try {
+    fs.writeFileSync(__dirname + path, content);
+  } catch (e) {
+        //this is probably fine, it may fail if the file already exists
+  }
+};
+
 module.exports = {
   copyFile,
+  mkFile,
+  mkDir,
 };
