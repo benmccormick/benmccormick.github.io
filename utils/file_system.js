1b3be3f6895e2c15daff0910e8a84f4ac3fd4ad4
Ben McCormick <ben.mccormick@windsorcircle.com>
2017-06-03 23:57:33 -0400

    Fix relative paths when working with the file system.  Have methods assume everything is relative to directory root

--- utils/file_system.js
+++ utils/file_system.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+
+const BASE_PATH = __dirname + '/..';
 // From http://stackoverflow.com/questions/11293857/fastest-way-to-copy-file-in-node-js
 const copyFile = (sourcePath, targetPath, cb) => {
   let cbCalled = false;
@@ -10,11 +12,11 @@ const copyFile = (sourcePath, targetPath, cb) => {
     }
   };
   //read stream
-  let rd = fs.createReadStream(__dirname + sourcePath);
+  let rd = fs.createReadStream(BASE_PATH + sourcePath);
   rd.on('error', err => done(err));
 
   //write stream
-  let wr = fs.createWriteStream(__dirname + targetPath);
+  let wr = fs.createWriteStream(BASE_PATH + targetPath);
   wr.on('error', err => done(err));
   wr.on('close', ex => done());
   rd.pipe(wr);
@@ -22,17 +24,19 @@ const copyFile = (sourcePath, targetPath, cb) => {
 
 const mkDir = path => {
   try {
-    fs.mkdirSync(__dirname + path);
+    fs.mkdirSync(BASE_PATH + path);
   } catch (e) {
         //this is probably fine, it may fail if the file already exists
+    console.log('oops');
   }
 };
 
 const mkFile = (path, content) => {
   try {
-    fs.writeFileSync(__dirname + path, content);
+    fs.writeFileSync(BASE_PATH + path, content);
   } catch (e) {
         //this is probably fine, it may fail if the file already exists
+    console.log(`🔥 Failed to write a file to ${path}, something is probably wrong`);
   }
 };
 
