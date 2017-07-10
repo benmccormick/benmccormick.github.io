96ffc41332b4191db993dbe9900fac1836201da9
Ben McCormick <ben.mccormick@windsorcircle.com>
2017-07-10 08:19:24 -0400

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-1327f22c85ebf1a110ae.js","14efb89e0a33ba0203748e8640cc048a"],["/commons-5ade941a0ddda2740b0b.js","8e06cf2bf195417f475ebad55e9febbb"],["/index.html","0036903d07ec53d3bfe2a50cea2bb395"],["/offline-plugin-app-shell-fallback/index.html","7ec54a00cda0238d48e6c2baa6a3c221"]];
+var precacheConfig = [["/app-1327f22c85ebf1a110ae.js","14efb89e0a33ba0203748e8640cc048a"],["/commons-5ade941a0ddda2740b0b.js","8e06cf2bf195417f475ebad55e9febbb"],["/index.html","2291ef368bf9d9b3dfc8962174f5900d"],["/offline-plugin-app-shell-fallback/index.html","df3678ada98ee7080358845f1a890486"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
