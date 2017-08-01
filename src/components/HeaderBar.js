6f23b0caba52f9390bc2e7c6b6208be6887ffa18
Ben McCormick <ben.mccormick@windsorcircle.com>
2017-07-31 23:27:15 -0400

    Experimental start for a header bar component in storybook

---
+++ src/components/HeaderBar.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import Link from 'gatsby-link';
+import '../css/header.css';
+
+export class HeaderBar extends React.Component {
+  render() {
+    let { blogTitle } = this.props;
+    return (
+      <div
+        style={{
+          display: 'flex',
+          // borderBottom: '1px solid rgba(200, 200, 200, 0.5)',
+          paddingBottom: '0.5rem',
+          marginBottom: '0.5rem',
+        }}
+      >
+        <div
+          style={{
+            flexGrow: 3,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+          }}
+        >
+          <h3
+            style={{
+              margin: 0,
+              paddingBottom: 0,
+            }}
+          >
+            <Link
+              style={{
+                boxShadow: 'none',
+                textDecoration: 'none',
+                color: 'inherit',
+              }}
+              name="title"
+              to={'/'}
+            >
+              {blogTitle}
+            </Link>
+          </h3>
+          <div
+            style={{
+              color: 'rgba(100,100,100, 0.7)',
+            }}
+          >
+            <Link className="header-link" to={'/subscribe/'}>
+              Subscribe
+            </Link>
+            <span style={{ padding: '0 0.33rem' }}>•</span>
+            <Link className="header-link" to={'/archive/'}>
+              Archive
+            </Link>
+            <span style={{ padding: '0 0.33rem' }}>•</span>
+            <a className="header-link" href="http://twitter.com/ben336">
+              Twitter
+            </a>
+            <span style={{ padding: '0 0.33rem' }}>•</span>
+            <Link className="header-link" to={'/about/'}>
+              About
+            </Link>
+          </div>
+        </div>
+        <div
+          style={{
+            flexShrink: 2,
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+HeaderBar.propTypes = {
+  blogTitle: React.PropTypes.string.isRequired,
+};
