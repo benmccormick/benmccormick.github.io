228e58fafe78eb3399460a51707c71c6f6018f5d
Ben McCormick <ben.mccormick@windsorcircle.com>
2017-09-16 23:36:24 -0400

    Cleanup category page with refactored method

--- src/templates/category-page.js
+++ src/templates/category-page.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import CategoryArchive from '../components/CategoryArchive';
-import get from 'lodash/get';
-import sortBy from 'lodash/sortBy';
+import { getSortedPosts } from '../utils/page-helpers';
 
 const CategoryPage = ({ data, pathContext }) => {
   const posts = data.allMarkdownRemark.edges;
-  const sortedPages = sortBy(posts, page => get(page, 'node.frontmatter.date'))
-    .reverse()
-    .filter(page => get(page, 'node.frontmatter.layout') === 'post')
-    .map(p => ({ data: p.node.frontmatter, path: p.node.fields.slug }));
+  const sortedPosts = getSortedPosts(posts);
   return (
-    <CategoryArchive pages={sortedPages} categoryKey={pathContext.category} />
+    <CategoryArchive pages={sortedPosts} categoryKey={pathContext.category} />
   );
 };
 
 CategoryPage.propTypes = {
-  route: React.PropTypes.object
+  route: React.PropTypes.object,
 };
 
 export default CategoryPage;
