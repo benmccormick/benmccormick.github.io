8efbab6492cb177e70a4f918560b4255b1b5081d
Ben McCormick <ben.mccormick@windsorcircle.com>
2017-12-30 18:21:16 -0500

    Improve home page styling

--- src/components/WelcomeBox.js
+++ src/components/WelcomeBox.js
@@ -4,26 +4,38 @@ import glamorous from 'glamorous';
 
 let Wrapper = glamorous.div({
   marginTop: '2rem',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
+  display: 'grid',
+  gridTemplateColumns: '80% 20%',
+  gridColumnGap: '1rem',
 });
 
 let ImageContainer = glamorous.div({
   maxWidth: '200px',
   maxHeight: '200px',
   marginRight: '1rem',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
 });
 
 let Image = glamorous.img({
   borderRadius: '50%',
 });
 
+let WelcomeContainer = glamorous.div({
+  maxWidth: '1000px',
+  margin: '1rem 0',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'flex-start',
+});
+
 export class WelcomeBox extends React.Component {
   render() {
     return (
       <Wrapper>
-        <div style={{ maxWidth: '1000px', margin: '1rem 0' }}>
+        <WelcomeContainer>
           <h1> Hi! I'm Ben. </h1>
           <p>
             {' '}I'm a software developer from Durham, North Carolina. I write
@@ -35,7 +47,7 @@ export class WelcomeBox extends React.Component {
             </a>{' '}
             or <a href="mailto:ben@benmccormick.org">email</a>.
           </p>
-        </div>
+        </WelcomeContainer>
         <ImageContainer className="no-mobile">
           <Image src="/headshot.jpeg" />
         </ImageContainer>
